83065698ad9b18e3e64df6881bc7d1fe742a2a99
Sourav Jain <159770782+code-with-souravjain@users.noreply.github.com>
2024-04-19 13:03:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let hi = document.querySelector("#hi");
+
+hi.addEventListener('click', function(){
+     hi.style.color = "blue"
+     hi.innerHTML = "byyyyyyy word"
+})
+
+
+var a = 5;
+
+console.log(a--);
+console.log(a);
+
